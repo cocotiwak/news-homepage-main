2ec5711d254e9937b9dfe5ec8fb57d1865954631
danoe <anggoro@gmail.com>
2023-12-21 01:57:18 +0700

    mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'Soft-orange': 'hsl(35, 77%, 62%)',
+        'SoftOrange': 'hsl(35, 77%, 62%)',
         'Soft-red': 'hsl(5, 85%, 63%)',
         'Off-white': 'hsl(36, 100%, 99%)',
         'Grayish-blue': 'hsl(233, 8%, 79%)',
         'Dark-grayish-blue': 'hsl(236, 13%, 42%)',
         'Very-dark-blue': 'hsl(240, 100%, 5%)',
+        'tomato': 'hsl(4, 100%, 67%)',
       },
       fontSize: {
         'paragraph': ['15px','24px'],
       },
       fontFamily: {
         'inter' : ['Inter','sans'],
-      }
+      },
+      textColor: {
+        'SoftOrange': 'hsl(35, 77%, 62%)',
+        'Soft-red': 'hsl(5, 85%, 63%)',
+        'Off-white': 'hsl(36, 100%, 99%)',
+        'Grayish-blue': 'hsl(233, 8%, 79%)',
+        'Dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'Very-dark-blue': 'hsl(240, 100%, 5%)',
+        'tomato': 'hsl(4, 100%, 67%)',
+      },
     },
   },
   variants: {
